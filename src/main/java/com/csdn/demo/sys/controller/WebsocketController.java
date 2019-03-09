2320cbfb237ef577d43c48e220bef1833fc5bfd2
1009342009@qq.com <cqn24680>
2019-03-09 12:41:13 +0800

    修改数据库信息

--- src/main/java/com/csdn/demo/sys/controller/WebsocketController.java
+++ src/main/java/com/csdn/demo/sys/controller/WebsocketController.java
@@ -13,9 +13,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
